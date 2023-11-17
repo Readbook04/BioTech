6af01b6d936ab05eb3f6c6ca89f31b61619b834f
YAD <123922738+Readbook04@users.noreply.github.com>
2023-11-16 21:06:18 -0600

    Delete vista directory

--- vista/modulos/banner.php
+++
@@ -1,9 +0,0 @@
-<!--BANNER INICIO-->
-<section class="banner">
-        <div class="content-banner">
-            <p>Sistema de riego para jardín Inteligente</p>
-            <h2>Descubre cómo la tecnología transforma tu jardín en un oasis inteligente.</h2>
-            <a href="../BioTech/Vista/html/productos.html">Comprar ahora</a>
-        </div>
-</section>
-    <!--BANNER FINAL-->
